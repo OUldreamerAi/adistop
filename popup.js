db13c4e63c868ab8d75aa9894461b12c313fc087
olli.becker <olli10856@gmail.com>
2026-02-10 20:31:30 +0200

    tring to get the exetension to know wich websites are open

--- popup.js
+++ popup.js
@@ -1,8 +1,17 @@
 
 document.getElementById("changeColorBtn").addEventListener("click", async () => {
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  chrome.tabs.sendMessage(tab.id, { action: "make_blue" }, (response) => {
+    chrome.tabs.sendMessage(tab.id, { action: "make_blue" }, (response) => {
     console.log("The page said:", response.status);
+
+    async function getCurrentTabUrl() {
+        let queryOptions = { active: true, currentWindow: true };
+        et [tab] = await chrome.tabs.query(queryOptions);
+        return tab?.url; // Palauttaa sivun koko URL-osoitteen
+    }
+
+
+
   });
 });
