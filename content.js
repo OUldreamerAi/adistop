bd2d572c292d22ffaa9b346339dd35a95b30e983
olli.becker <olli10856@gmail.com>
2026-02-11 12:26:50 +0200

    created the opup which blocks the screen if you entered certain websites

--- content.js
+++ content.js
@@ -10,10 +10,14 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 const currentUrl = window.location.href;
 
-if (currentUrl.includes("youtube.com")) {
-    modal.style.display = "block";
+if (currentUrl.includes("example.com" || "https://www.kali.org/get-kali/#kali-platforms")) {
+    const overlay = document.createElement("div");
+    const badge = document.createElement("p");
+    badge.textContent = `⏱️ uhweguiyrtwq vt2    uhg 2ty o8rvt gu24quyiogr4ucivgqycr4yh8ghc4rgyr 4gy7cqrt34cr4gcr4guytgucqrwgiuyqcgiyrwcf tgiy min read`;
+    overlay.appendChild(badge);
+    document.body.appendChild(overlay);
 }
 
 //makes white box where wikibedia article
 //Ai made questions avbout set wikibedia article
-//wikibedia
+//wikibedia article is about a topic and the questions are about the topic
--- popup.js
+++ popup.js
@@ -1,21 +1,3 @@
 
-document.getElementById("changeColorBtn").addEventListener("click", async () => {
-    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-    chrome.tabs.sendMessage(tab.id, { action: "make_blue" }, (response) => {
-    console.log("The page said:", response.status);
-
-  });
-});
-
-// Function to check the active tab
-function checkActiveTab() {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    if (tabs[0] && tabs[0].url) {
-      console.log("Current URL: ", tabs[0].url);
-      // You can send this data to a local variable,
-      // storage, or an external server here.
-    }
-  });
-}
 
