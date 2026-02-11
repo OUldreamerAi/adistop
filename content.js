51a458a3c16a2b52d37aa232e835a528a15ba48d
olli.becker <olli10856@gmail.com>
2026-02-11 12:49:52 +0200

    trying to get the webiste be blocker in a way thats good enough

--- content.js
+++ content.js
@@ -1,5 +1,4 @@
 
-var modal = document.getElementById("myModal");
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === 'make_blue') {
@@ -10,8 +9,16 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 const currentUrl = window.location.href;
 
-if (currentUrl.includes("example.com" || "https://www.kali.org/get-kali/#kali-platforms")) {
+if (currentUrl.includes("example.com") || currentUrl.includes("https://www.kali.org/get-kali/#kali-platforms")) {
+
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = chrome.runtime.getURL("popup.css"); // Adjust this path if necessary
+
+    document.head.appendChild(link);
+    
     const overlay = document.createElement("div");
+    overlay.classList.add("overlay");
     const badge = document.createElement("p");
     badge.textContent = `⏱️ uhweguiyrtwq vt2    uhg 2ty o8rvt gu24quyiogr4ucivgqycr4yh8ghc4rgyr 4gy7cqrt34cr4gcr4guytgucqrwgiuyqcgiyrwcf tgiy min read`;
     overlay.appendChild(badge);
